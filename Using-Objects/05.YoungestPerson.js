f68c44b0381d012d448143e6fa7faf2929b97898
Dimitar Mitev <dnmitev@gmail.com>
2014-05-12 17:08:08 +0300

    using objects hw is done

---
+++ Using-Objects/05.YoungestPerson.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    "use strict";
+
+    function createPerson(firstName, lastName, age) {
+        return {
+            firstName: firstName,
+            lastName: lastName,
+            age: age
+        };
+    }
+
+    function getTheYoungestPersonFullName(arr) {
+        var i,
+            min = 1000,
+            len = arr.length,
+            fullName = "";
+
+        for (i = 0; i < len; i += 1) {
+            if (arr[i].age < min) {
+                min = arr[i].age;
+                fullName = arr[i].firstName + " " + arr[i].lastName;
+            }
+        }
+
+        return fullName;
+    }
+
+    var people = [
+        createPerson("Bat", "Georgi", 23),
+        createPerson("Dinko", "Zadushavam sa", 12),
+        createPerson("Pesho", "the One", 25),
+        createPerson("Unufri", "Picha", 27)
+    ];
+
+    jsConsole.writeLine(getTheYoungestPersonFullName(people) + " is the youngest person.");
+})();
---
+++ Using-Objects/06.GroupByProperty.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    "use strict";
+
+    function groupByKey(people, key) {
+        var i,
+            groupedPeople = {},
+            groupName,
+            currPerson,
+            len = people.length;
+
+        for (i = 0; i < len; i += 1) {
+            groupName = people[i][key];
+            currPerson = people[i];
+
+            if (groupedPeople[groupName]) {
+                groupedPeople[groupName].push(currPerson);
+            } else {
+                groupedPeople[groupName] = currPerson;
+            }
+        }
+
+        return groupedPeople;
+    }
+
+    function createPerson(firstName, lastName, age) {
+        return {
+            firstName: firstName,
+            lastName: lastName,
+            age: age
+        };
+    }
+
+    var groupedPeople,
+        people = [
+             createPerson("Bat", "Georgi", 23),
+             createPerson("Dinko", "Zadushavam sa", 12),
+             createPerson("Pesho", "the One", 25),
+             createPerson("Unufri", "Picha", 27)
+        ];
+
+    groupedPeople = groupByKey(people, "firstName");
+
+    for (var group in groupedPeople) {
+        jsConsole.writeLine("Group: " + group);
+
+        for (var member in groupedPeople[group]) {
+            jsConsole.writeLine("---Item:" + groupedPeople[group][member]);
+        }
+    }
+})();
